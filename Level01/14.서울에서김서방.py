8e1c40fbd859041da4fe0640c1b31af64ec0665b
jongdu <parksn5029@naver.com>
2021-07-06 22:43:07 +0900

    study Level01 07.06

--- Level01/14.서울에서김서방.py
+++
@@ -1,8 +0,0 @@
-def solution(seoul):
-    c = seoul.index('Kim')
-    result = "김서방은 " + str(c) + " 에 있다"
-    return result
-
-seoul = ["Jane", "Kim"]
-seoul = solution(seoul)
-print(seoul)
--- Level01/17.자릿수더하기.py
+++
@@ -1,6 +0,0 @@
-def solution(n):
-    return sum(list(map(int, str(n))))
-
-n = 123
-n = solution(n)
-print(n)
