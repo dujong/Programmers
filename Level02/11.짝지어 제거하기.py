eb65efb11c1716c7ea1f92601f96891d1b1704a9
jongdu <parksn5029@naver.com>
2021-07-18 16:54:13 +0900

    Stduy Level02 07.18

---
+++ Level02/11.짝지어 제거하기.py
@@ -0,0 +1,20 @@
+def solution(s):
+    stack = []
+
+    for i in s:
+        if stack == []:
+            stack.append(i)
+        elif stack[-1] == i:
+            stack.pop()
+        else:
+            stack.append(i)
+
+    if stack == []:
+        return 1
+    else:
+        return 0
+    
+
+s = 'baabaa'
+
+print(solution(s))
