94f9ed4216c4cac058f12c0f1fae2df6e535e295
jongdu <parksn5029@naver.com>
2021-11-12 17:10:41 +0900

    그리디 알고리즘 문제풀이

---
+++ 그리디/2.큰수만들기.py
@@ -0,0 +1,10 @@
+# 스택을 사용해서 다시 풀어보기
+               
+        
+number = "9998887776665554443332221110009"
+k = 30
+#print(solution(number, k))
+
+print('9' * 4)
+# number = '1914'
+# print(''.join(sorted(number, reverse=True)))
