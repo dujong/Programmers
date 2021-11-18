8964e5915e97490869e9311ddd538d107127c443
jongdu <parksn5029@naver.com>
2021-11-18 11:28:40 +0900

    Study 그리디 11.18

--- 그리디/4.섬연결하기.py
+++ 그리디/4.섬연결하기.py
@@ -1,23 +1,19 @@
 def solution(n, costs):
-    answer = 0
-    u = [False] * n
-    costs.sort(key=lambda x : x[2])
+    # kruskal algorithm
+    ans = 0
+    costs.sort(key = lambda x: x[2])
+    routes = set([costs[0][0]])
     
-    for i1, i2, cost in costs:
-        
-        if u[i1] == False or u[i2] == False:
-            answer += cost
-            u[i1] = True
-            u[i2] = True
+    while len(routes)!=n:
+        for i, cost in enumerate(costs):
             
-        if sum(u) == n:
-            return answer
-    
-n = 6
-costs = [[0, 1, 5], [0, 3, 2], [0, 4, 3], [1, 4, 1], [3, 4, 10], [1, 2, 2], [2, 5, 3], [4, 5, 4]]
-costs.sort(key=lambda x : x[2])
-print(costs)
-print(solution(n, costs))
-
-# n개의 False 리스트를 만든 후 costs를 정렬하여 연결되는 부분을 True로
-# 앞과 뒤중에 False가 있으면 cost를 더하고 연결
+            if cost[0] in routes and cost[1] in routes:
+                continue
+                
+            if cost[0] in routes or cost[1] in routes:
+                routes.update([cost[0], cost[1]])
+                ans += cost[2]
+                costs[i] = [-1, -1, -1]
+                break
+                
+    return ans
